a195aba12dac7e772a790690cbd968f3a6734109
19127406 <63275057+19127406@users.noreply.github.com>
2022-06-11 13:40:45 +0700

    Still same problem

--- UI.py
+++ UI.py
@@ -4,46 +4,52 @@ from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QStackedWidget
 from PyQt5.uic import loadUi
 
 class LoginWindow(QDialog):
-    def __init__(self) -> None:
+    def __init__(self, w, h) -> None:
         super(LoginWindow, self).__init__()
         loadUi("login_ui.ui", self)
         self.setWindowTitle("Application")
         self.LoginBtn.clicked.connect(self.LoginClicked)
         self.OpenRegister.clicked.connect(self.OpenRegisterClicked)
 
+        self.setFixedSize(w, h)
+
     def LoginClicked(self):
         inputmail = self.inputmain.text()
         inputpasswd = self.inputpasswd.text()
 
     def OpenRegisterClicked(self):
-        reg = RegisterWindow()
+        reg = RegisterWindow(488, 637)
         widgets.addWidget(reg)
         widgets.setCurrentIndex(widgets.currentIndex() + 1)
         print(widgets.currentIndex())
 
 class RegisterWindow(QDialog):
-    def __init__(self) -> None:
+    def __init__(self, w, h) -> None:
         super(RegisterWindow, self).__init__()
         loadUi("register_ui.ui", self)
         self.RegisterBtn.clicked.connect(self.RegisterClicked)
         self.CancelBtn.clicked.connect(self.CancelClicked)
+
+        self.setFixedSize(w, h)
         
     def RegisterClicked(self):
         pass
 
     def CancelClicked(self):
-        mainwindow = LoginWindow()
+        mainwindow = LoginWindow(488, 341)
+        mainwindow.setFixedSize(488, 341)
         widgets.addWidget(mainwindow)
-        widgets.setCurrentIndex(widgets.currentIndex() + 1)
+        widgets.setCurrentIndex(widgets.currentIndex() - 1)
         print(widgets.currentIndex())
 
 
 # Initiate application
 app = QApplication(sys.argv)
-mainwindow = LoginWindow()
+mainwindow = LoginWindow(488, 341)
 
 widgets = QStackedWidget()
 widgets.addWidget(mainwindow)
+# widgets.setFixedSize(488, 341)
 widgets.show()
 
 app.exec_()
