eb56479509652e433c9fd6011ff41a2c2165f591
19127406 <63275057+19127406@users.noreply.github.com>
2022-06-19 17:13:31 +0700

    login & register

--- UI.py
+++ UI.py
@@ -1,55 +1,131 @@
 import sys
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QStackedWidget
-from PyQt5.uic import loadUi
+import PyQt5
+from PyQt5.QtWidgets import *
+
+from UserFunctions import login, register
+
 
 class LoginWindow(QDialog):
-    def __init__(self, w, h) -> None:
-        super(LoginWindow, self).__init__()
-        loadUi("login_ui.ui", self)
-        self.setFixedSize(w, h)
-        self.setWindowTitle("Application")
-        self.LoginBtn.clicked.connect(self.LoginClicked)
-        self.OpenRegister.clicked.connect(self.OpenRegisterClicked)
+    def __init__(self) -> None:
+        super().__init__()
+        self.dlgLayout = QVBoxLayout()
+        # Create a form layout and add widgets
+        self.formLayout = QFormLayout()
 
+        self.email = QLineEdit()
+        self.passwd = QLineEdit()
+        self.passwd.setEchoMode(QLineEdit.EchoMode.Password)
 
+        self.formLayout.addRow("Email:", self.email)
+        self.formLayout.addRow("Password:", self.passwd)
+
+        # Add a button box
+        btnBox = QDialogButtonBox()
+
+        self.login_btn = QPushButton("OK")
+        self.login_btn.clicked.connect(self.LoginClicked)
+        btnBox.addButton(self.login_btn, QDialogButtonBox.AcceptRole)
+
+        self.cancel_btn = QPushButton("Cancel")
+        self.cancel_btn.clicked.connect(self.CancelClicked)
+        btnBox.addButton(self.cancel_btn, QDialogButtonBox.RejectRole)
+
+        # Set the layout on the dialog
+        self.dlgLayout.addLayout(self.formLayout)
+        self.dlgLayout.addWidget(btnBox)
+        self.setLayout(self.dlgLayout)
 
     def LoginClicked(self):
-        inputmail = self.inputmain.text()
-        inputpasswd = self.inputpasswd.text()
-
-    def OpenRegisterClicked(self):
-        reg = RegisterWindow(488, 637)
-        widgets.addWidget(reg)
-        widgets.setCurrentIndex(widgets.currentIndex() + 1)
-        print(widgets.currentIndex())
-
-class RegisterWindow(QDialog):
-    def __init__(self, w, h) -> None:
-        super(RegisterWindow, self).__init__()
-        loadUi("register_ui.ui", self)
-        self.setFixedSize(w, h)
-        self.RegisterBtn.clicked.connect(self.RegisterClicked)
-        self.CancelBtn.clicked.connect(self.CancelClicked)
-
-        
-    def RegisterClicked(self):
         pass
 
     def CancelClicked(self):
-        mainwindow = LoginWindow(488, 341)
-        widgets.addWidget(mainwindow)
-        widgets.setCurrentIndex(widgets.currentIndex() - 1)
-        print(widgets.currentIndex())
+        app.quit()
 
 
-# Initiate application
-app = QApplication(sys.argv)
-mainwindow = LoginWindow(488, 341)
+class RegisterWindow(QDialog):
+    def __init__(self) -> None:
+        super().__init__()
+        self.dlgLayout = QVBoxLayout()
+        # Create a form layout and add widgets
+        self.formLayout = QFormLayout()
 
-widgets = QStackedWidget()
-widgets.addWidget(mainwindow)
-# widgets.setFixedSize(488, 341)
-widgets.show()
+        self.email = QLineEdit()
+        self.fullname = QLineEdit()
+        self.birthday = QDateEdit()
+        self.phonenumber = QLineEdit()
+        self.address = QLineEdit()
+        self.passwd = QLineEdit()
+        self.passwd.setEchoMode(QLineEdit.EchoMode.Password)
+        self.passwd_again = QLineEdit()
+        self.passwd_again.setEchoMode(QLineEdit.EchoMode.Password)
 
-app.exec_()
+        self.formLayout.addRow("Email:", self.email)
+        self.formLayout.addRow("Fullname:", self.fullname)
+        self.formLayout.addRow("Birthday:", self.birthday)
+        self.formLayout.addRow("Phone:", self.phonenumber)
+        self.formLayout.addRow("Address:", self.address)
+        self.formLayout.addRow("Password:", self.passwd)
+        self.formLayout.addRow("Password again:", self.passwd_again)
+
+        # Add a button box
+        btnBox = QDialogButtonBox()
+
+        self.login_btn = QPushButton("OK")
+        self.login_btn.clicked.connect(self.RegisterClicked)
+        btnBox.addButton(self.login_btn, QDialogButtonBox.AcceptRole)
+
+        self.cancel_btn = QPushButton("Cancel")
+        self.cancel_btn.clicked.connect(self.CancelClicked)
+        btnBox.addButton(self.cancel_btn, QDialogButtonBox.RejectRole)
+
+        # Set the layout on the dialog
+        self.dlgLayout.addLayout(self.formLayout)
+        self.dlgLayout.addWidget(btnBox)
+        self.setLayout(self.dlgLayout)
+
+    def RegisterClicked(self):
+        if self.passwd.text != self.passwd_again.text:
+            QMessageBox.about(self, "Error", "Re-entered password is incorrect")
+            self.passwd.text = ""
+            self.passwd_again.text = ""
+        else:
+            reg = register(self.email.text, self.passwd.text, self.fullname.text, self.birthday.text, self.phonenumber.text, self.address.text)
+            if reg == False:
+                QMessageBox.about(self, "Error", "Register unsuccessful (Unexpected error occured)!!!")
+            else:
+                QMessageBox.about(self, "Success", "Successfully register")
+
+    def CancelClicked(self):
+        app.quit()
+
+
+class FirstWindow(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle("Darkhold")
+        self.resize(400, 300)
+        self.layout = QVBoxLayout()
+
+        self.login = LoginWindow()
+        self.register = RegisterWindow()
+
+        self.tabs = QTabWidget()
+        self.tabs.addTab(self.login, "Login")
+        self.tabs.addTab(self.register, "Register")
+
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.first = FirstWindow()
+        self.first.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    execute = MainWindow()
+    sys.exit(app.exec_())
